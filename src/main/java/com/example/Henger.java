34e4ab355216378f19db333a6f41821efd658e40
Lapis Péter <lapispeter69@gmail.com>
2026-03-31 18:19:43 +0200

    számolás kész

--- src/main/java/com/example/Henger.java
+++ src/main/java/com/example/Henger.java
@@ -1,5 +1,9 @@
 package com.example;
 
 public class Henger {
-    
+    public static double calcArea(double sugar, double magassag) {
+        
+        double area=(2 * Math.PI * sugar) * (sugar + magassag);
+        return area;
+    }
 }
---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField areaField;
+
+    @FXML
+    private TextField magassagField;
+
+    @FXML
+    private TextField sugarField;
+
+    @FXML
+    void onClickCalcButton(ActionEvent event) {
+         startCalc();
+    }
+
+    void startCalc() {
+        double sugar=Double.parseDouble(sugarField.getText());
+        double magassag=Double.parseDouble(magassagField.getText());
+        
+
+
+        Double area= Henger.calcArea(sugar, magassag);
+        areaField.setText(area.toString());
+        //Storage.writeContent(area.toString());
+    }
+
+}
